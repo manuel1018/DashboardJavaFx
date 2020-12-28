6e191cf481f355c0d4edf005621fa304a6441e14
manuel1018 <juanma_1018@hotmail.com>
2020-12-28 04:06:09 -0600

    Modification. Second commit

---
+++ src/application/models/Instructor.java
@@ -0,0 +1,9 @@
+package application.models;
+
+import java.time.LocalDate;
+
+public class Instructor extends PersonData {
+	   public int idInstructor;
+       private double Salary ;
+       private int workinghours;
+}
---
+++ src/application/models/PersonData.java
@@ -0,0 +1,14 @@
+package application.models;
+
+import java.time.LocalDate;
+
+public class PersonData {
+	public String name;
+    public String lastName ;
+    public String gender ;
+    public int age ;
+    public LocalDate dateOfBirth;
+    public int telephoneNumber ;
+    public String medicalConditions;
+    public String email;
+}
